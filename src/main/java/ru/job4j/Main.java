512d93d0a2482b76d6d09249658fc4c152c74947
Sarmit <OlgertErmakov@gmail.com>
2022-05-15 20:37:03 +0400

    0. Что такое переменная. [#3610]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -2,6 +2,8 @@ package ru.job4j;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int age = 18;
+        float length = 78.0332f;
+        boolean baby = true;
     }
 }
