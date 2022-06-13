eba13d2759992fa165342f8251a74a84678d411b
Sarmit <OlgertErmakov@gmail.com>
2022-06-13 18:48:35 +0400

    4.1. Аргументы [#295521]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -2,8 +2,8 @@ package ru.job4j;
 
 public class Main {
     public static void main(String[] args) {
-        int age = 18;
-        float length = 78.0332f;
-        boolean baby = true;
+        int size = 10;
+        size = 100;
+
     }
 }
