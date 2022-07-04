814b997e3b2c7ccde858676c9bf02d0bca418dea
Sarmit <OlgertErmakov@gmail.com>
2022-07-04 22:27:31 +0400

    1.2. Входные данные в тесте [#297750]

---
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -0,0 +1,19 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class X2Test {
+
+    @Test
+    public void whenA1B1C1X1Then3() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 3;
+        int rsl = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, rsl);
+    }
+
+}
