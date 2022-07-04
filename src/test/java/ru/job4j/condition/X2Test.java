66bf0b10692dec10e76a6b367296263542328f0d
Sarmit <OlgertErmakov@gmail.com>
2022-07-04 22:37:07 +0400

    1.2. Входные данные в тесте [#297750 #318805]

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -6,10 +6,10 @@ import org.junit.Test;
 public class X2Test {
 
     @Test
-    public void whenA0B1C1X1Then3() {
-        int a = 0;
+    public void whenA1B1C0X1Then3() {
+        int a = 1;
         int b = 1;
-        int c = 1;
+        int c = 0;
         int x = 1;
         int expected = 2;
         int rsl = X2.calc(a, b, c, x);
